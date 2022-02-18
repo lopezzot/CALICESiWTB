85d3c43157d817cdef83ed9cec51f4cc5eb1d2a7
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2022-02-18 17:32:42 +0100

    prepare for sensitive detector

--- src/CaliceDetectorConstruction.cc
+++ src/CaliceDetectorConstruction.cc
@@ -44,65 +44,43 @@ void CaliceDetectorConstruction::ConstructSDandField() {
  
     //G4cout << "Constructing Sensitive Detector..." << G4endl; 
     ///////////////////////////////////////////////////////////////////////
-    //
+
     // Example how to retrieve Auxiliary Information for sensitive detector
     //
-  /*const G4GDMLAuxMapType* auxmap = fParser.GetAuxMap();
-  G4cout << "Found " << auxmap->size()
-            << " volume(s) with auxiliary information."
-            << G4endl << G4endl;
-  for(G4GDMLAuxMapType::const_iterator iter=auxmap->begin();
-      iter!=auxmap->end(); iter++) 
-  {
-    G4cout << "Volume " << ((*iter).first)->GetName()
+    const G4GDMLAuxMapType* auxmap = fParser.GetAuxMap();
+    G4cout << "Found " << auxmap->size()
+           << " volume(s) with auxiliary information."
+           << G4endl << G4endl;
+    for(G4GDMLAuxMapType::const_iterator iter=auxmap->begin(); iter!=auxmap->end(); iter++) {
+        G4cout << "Volume " << ((*iter).first)->GetName()
            << " has the following list of auxiliary information: "
            << G4endl << G4endl;
-    for (G4GDMLAuxListType::const_iterator vit=(*iter).second.begin();
-         vit!=(*iter).second.end(); vit++)
-    {
-      G4cout << "--> Type: " << (*vit).type
-                << " Value: " << (*vit).value << G4endl;
+        for (G4GDMLAuxListType::const_iterator vit=(*iter).second.begin(); vit!=(*iter).second.end(); vit++) {
+            G4cout << "--> Type: " << (*vit).type
+                   << " Value: " << (*vit).value << G4endl;
+        }
     }
-  }
-  G4cout << G4endl;*/
+    G4cout<<G4endl;
 
-  // The same as above, but now we are looking for
-  // sensitive detectors setting them for the volumes
-/*
-  for(G4GDMLAuxMapType::const_iterator iter=auxmap->begin();
-      iter!=auxmap->end(); iter++) 
-  {
-    G4cout << "Volume " << ((*iter).first)->GetName()
-           << " has the following list of auxiliary information: "
-           << G4endl << G4endl;
-    for (G4GDMLAuxListType::const_iterator vit=(*iter).second.begin();
-         vit!=(*iter).second.end();vit++)
-    {
-      if ((*vit).type=="SensDet")
-      {
-        G4cout << "Attaching sensitive detector " << (*vit).value
-               << " to volume " << ((*iter).first)->GetName()
-               <<  G4endl << G4endl;
+    // The same as above, but now we are looking for
+    // sensitive detectors setting them for the volumes
+    //
+    for(G4GDMLAuxMapType::const_iterator iter=auxmap->begin(); iter!=auxmap->end(); iter++) {
+        G4cout << "Volume " << ((*iter).first)->GetName()
+               << " has the following list of auxiliary information: "
+               << G4endl << G4endl;
+        for (G4GDMLAuxListType::const_iterator vit=(*iter).second.begin(); vit!=(*iter).second.end();vit++) {
+            if ((*vit).type=="SensDet") {
+                G4cout << "Attaching sensitive detector " << (*vit).value
+                << " to volume " << ((*iter).first)->GetName()
+                <<  G4endl << G4endl;
 
-        //G4VSensitiveDetector* mydet = 
-         // SDman->FindSensitiveDetector((*vit).value);
-*/  
-    /* if(mydet) 
-        {*/
-    /*
-         G4LogicalVolume* myvol = (*iter).first;
-        //if ((*vit).value=="WAbsorber")  myvol->SetSensitiveDetector(absoSD);
-         //if ((*vit).value=="WAbsorber")  continue;
-        //else if ((*vit).value=="SiActive") myvol->SetSensitiveDetector(activeSD);
-         myvol->SetSensitiveDetector(ecalSD);
-        //}
-        */
-        /*else
-        {
-          G4cout << (*vit).value << " detector not found" << G4endl;
-        }*/
-      /*}
+                G4LogicalVolume* myvol = (*iter).first;
+                //myvol->SetSensitiveDetector(ecalSD);
+            }
+        }
     }
-  }*/
+
 }
 
+//**************************************************
