1e90e34335e1fac3a1cd4bdad5dd561d765594f6
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2022-02-28 16:32:47 +0100

    same output name from G4Analysis

--- src/CaliceRunAction.cc
+++ src/CaliceRunAction.cc
@@ -68,8 +68,7 @@ void CaliceRunAction::BeginOfRunAction(const G4Run* aRun) {
     auto analysisManager = G4AnalysisManager::Instance();
     //std::string runnumber = std::to_string( aRun->GetRunID() );
     //G4String outputfile = "CALICESiWTBout_Run"+runnumber;
-    G4String outputfile = "CALICESiWTBout";
-    //G4String outputfile = fPhysList; 
+    G4String outputfile = fPhysList; 
     analysisManager->OpenFile( outputfile );
 
 }
