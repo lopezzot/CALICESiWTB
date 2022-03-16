9b2aea1a8364183dc2225cc3e2ab9f7709b62de2
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2022-03-16 13:51:09 +0100

    add v0.3 analysis

---
+++ analysis/v0.3/AnalysisHelper.h
@@ -0,0 +1,36 @@
+
+void myGraphSetUp(TGraph *graph, Color_t color, int style){
+  graph->SetMarkerStyle(20);
+  graph->SetMarkerColor(color);
+  graph->SetLineColor(color);
+  graph->SetLineWidth(3);
+  graph->SetLineStyle(style);
+  return;
+}
+
+void myTopPadSetUp(TPad *currentPad, float currentLeft=0.15, float currentTop=0.05/0.6, float currentRight=0.05, float currentBottom=0.00){
+  currentPad->SetLeftMargin(currentLeft);
+  currentPad->SetTopMargin(currentTop);
+  currentPad->SetRightMargin(currentRight);
+  currentPad->SetBottomMargin(currentBottom);
+  currentPad->SetFillColor(10);
+  return;
+}
+
+void myBottomPadSetUp(TPad *currentPad, float currentLeft=0.15, float currentTop=0.00, float currentRight=0.05, float currentBottom=0.14/0.4){
+  currentPad->SetLeftMargin(currentLeft);
+  currentPad->SetTopMargin(currentTop);
+  currentPad->SetRightMargin(currentRight);
+  currentPad->SetBottomMargin(currentBottom);
+  currentPad->SetFillColor(10);
+  return;
+}
+
+void myPadSetUp(TPad *currentPad, float currentLeft=0.15, float currentTop=0.05, float currentRight=0.05, float currentBottom=0.14){
+  currentPad->SetLeftMargin(currentLeft);
+  currentPad->SetTopMargin(currentTop);
+  currentPad->SetRightMargin(currentRight);
+  currentPad->SetBottomMargin(currentBottom);
+  currentPad->SetFillColor(10);
+  return;
+}
