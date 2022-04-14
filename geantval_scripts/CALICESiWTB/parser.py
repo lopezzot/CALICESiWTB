3ab6b6a98e5e76be80c98e422604da48e38c74df
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2022-04-14 10:27:53 +0200

    small change in parser

--- geantval_scripts/CALICESiWTB/parser.py
+++ geantval_scripts/CALICESiWTB/parser.py
@@ -12,10 +12,7 @@
 import os
 import sys
 import json
-import numpy as np
 
-from ROOT import gROOT, TFile, TTree, TCanvas, gStyle
-from ROOT import TH1F, TF1
 from gts.utils import getJSON
 from gts.BaseParser import BaseParser, mergeROOT, mktempdir
 
@@ -63,7 +60,7 @@ class Test(BaseParser):
 	    #Find job with corresponding energy
 	    job = [x for x in jobs if int(x["ENERGY"])==energy][0]
 	    path = job["path"]
-	    infile = TFile.Open(os.path.join(job["path"],"CALICESiWTBout.root"))
+	    #infile = TFile.Open(os.path.join(job["path"],"CALICESiWTBout.root"))
 	    root_command = "cd " + job["path"] + ";root -l 'MyLongitudinalPlots.C(" + str(counter) + "," + "\"CALICESiWTBout.root\")'"
 	    #print root_command
 	    #os.system(root_command)
@@ -71,7 +68,7 @@ class Test(BaseParser):
 	    yhit_values = GetObservables(path+"/"+str(energy)+"GeVhit.txt")
 	    print "--->energy values: " + str(yenergy_values) + " ,physlist: " + str(set([x["PHYSLIST"] for x in jobs]))
 	    print "--->hit values: " + str(yhit_values) + " ,physlist: " + str(set([x["PHYSLIST"] for x in jobs]))
-            '''
+            
 	    #Create JSON output files for energy (graph)
 	    #
 	    yield getJSON(jobs[0], "chart",
@@ -108,10 +105,10 @@ class Test(BaseParser):
 	                          yStatErrorsMinus=ystaterrorMC_layer,
 				  yStatErrorsPlus=ystaterrorMC_layer
                                   )
-            '''
+           
 	    #Part for data json creation (extract FTFP_BERT phys list jobs)
-	    #if jobs[0]["PHYSLIST"]=="FTFP_BERT":
-	    if True:
+	    '''
+	    if str(jobs[0]["PHYSLIST"])=="FTFP_BERT":
 		y_energy_data, y_energy_data_error = GetDataObservables(path+"/"+str(energy)+"GeVenergydata.txt")
 		y_hit_data, y_hit_data_error = GetDataObservables(path+"/"+str(energy)+"GeVhitdata.txt")
 		
@@ -120,6 +117,8 @@ class Test(BaseParser):
 		yield getJSON(jobs[0], "chart",
 		                      mctool_name="experiment",
 			              mctool_model="experiment",
+				      mctool_version="experiment",
+				      testName="experiment",
 			              observableName="energy per layer",
 				      #secondaryParticle="e-",
 				      beamParticle="pi-",
@@ -138,6 +137,8 @@ class Test(BaseParser):
 		yield getJSON(jobs[0], "chart",
 		                      mctool_name="experiment",
 			              mctool_model="experiment",
+				      mctool_version="experiment",
+				      testName="experiment",
 			              observableName="hit per layer",
 				      #secondaryParticle="e-",
 				      beamParticle="pi-",
@@ -151,5 +152,5 @@ class Test(BaseParser):
 	                              yStatErrorsMinus=y_hit_data_error,
 				      yStatErrorsPlus=y_hit_data_error
                                       )
-	    
+	    ''' 
 ##**************************************************
